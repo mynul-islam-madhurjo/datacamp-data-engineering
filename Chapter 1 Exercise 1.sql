bfaebffdab7fd696c331c63582d78d40f0d2d315
mynul-islam-madhurjo <mynul.islam@g.bracu.ac.bd>
2023-08-30 00:07:22 +0600

    c1 e1 select query

---
+++ Chapter 1 Exercise 1.sql
@@ -0,0 +1,5 @@
+-- We've set up a database inside this course, and the books table is available in the exercise. You'll use SQL to query this table in the next chapter, but for now, it's time to explore what data books holds!
+
+-- Your task is to choose the option below that best describes the information contained in books. There's no need to do any coding in this exercise; you can answer this question by looking at the books table in the console next to the words "query result." Because some book titles are long, you may need to scroll to the right in order to see all the information that the books table contains.
+
+select * from books;
