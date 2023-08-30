1b51fe34f44ff1438443b320360d15ed2f5a33de
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-08-30 09:26:35 +0600

    select from queries

---
+++ Chapter 2 Exercise 2/Chapter 2 Exercise 2.sql
@@ -0,0 +1,11 @@
+-- Use SQL to return a result set of all book titles included in the books table.
+
+select title from books;
+
+-- Select title and author from the books table
+SELECT title,author
+FROM books;
+
+-- Select all fields from the books table
+SELECT *
+FROM books;
