0f8565f92fd733749f3d55cc511e7e1622027941
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-03 03:03:40 +0600

    Query with AND OR BETWEEN

---
+++ Intermediate SQL/Chapter 2/Exercise 2 Between And Or.sql
@@ -0,0 +1,73 @@
+--AND
+-- Select the title and release_year for all German-language films released before 2000
+SELECT 
+    title,
+    release_year
+FROM films
+WHERE language = 'German'
+    AND release_year < 2000;
+
+-- Update the query to see all German-language films released after 2000
+SELECT 
+    title,
+    release_year
+FROM films
+WHERE language = 'German'
+    AND release_year > 2000;
+
+-- Select all records for German-language films released after 2000 and before 2010
+SELECT * 
+FROM films
+WHERE language = 'German' 
+    AND release_year < 2010 AND release_year > 2000;  
+
+--OR
+-- Find the title and year of films from the 1990 or 1999
+SELECT 
+    title,
+    release_year
+FROM films
+WHERE release_year = 1990
+    OR release_year = 1999;
+
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+-- Add a filter to see only English or Spanish-language films
+AND (language = 'English' OR language = 'Spanish');
+
+SELECT title, release_year
+FROM films
+WHERE (release_year = 1990 OR release_year = 1999)
+	AND (language = 'English' OR language = 'Spanish')
+-- Filter films with more than $2,000,000 gross
+	AND gross > 2000000;
+
+--BETWEEN
+-- Select the title and release_year for films released between 1990 and 2000
+SELECT
+    title,
+    release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000; 
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+-- Narrow down your query to films with budgets > $100 million
+	AND budget > 100000000;
+
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Restrict the query to only Spanish-language films
+	AND language = 'Spanish';
+
+SELECT title, release_year
+FROM films
+WHERE release_year BETWEEN 1990 AND 2000
+	AND budget > 100000000
+-- Amend the query to include Spanish or French-language films
+	AND (language = 'Spanish' OR language = 'French');
