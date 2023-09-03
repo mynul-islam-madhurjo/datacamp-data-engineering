d05e4cd2e2d7e21f4ad1733d5e54f57446e545aa
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-03 18:03:15 +0600

    using aggregate function with filter in query

---
+++ Intermediate SQL/Chapter 3/Exercise 2 Agg Function with filter.sql
@@ -0,0 +1,27 @@
+-- Calculate the sum of gross from the year 2000 or later
+SELECT SUM(gross) AS total_gross
+FROM films
+WHERE release_year >= 2000;
+
+-- Calculate the average gross of films that start with A
+SELECT AVG(gross) AS avg_gross_A
+FROM films
+WHERE title LIKE 'A%';
+
+-- Calculate the lowest gross film in 1994
+SELECT MIN(gross) AS lowest_gross
+FROM films
+WHERE release_year = 1994;
+
+-- Calculate the highest gross film released between 2000-2012
+SELECT MAX(gross) AS highest_gross
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
+
+-- Round the average number of facebook_likes to one decimal place
+SELECT ROUND(AVG(facebook_likes),1) AS avg_facebook_likes
+FROM reviews;
+
+-- Calculate the average budget rounded to the thousands
+SELECT ROUND(AVG(budget),-3) AS avg_budget_thousands
+FROM films;
