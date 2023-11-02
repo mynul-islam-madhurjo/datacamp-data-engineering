3cf1062a90b9f48aa282faa58239a6cd9e660b06
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-11-02 15:32:16 +0600

    Course 4 Chapter 3 forign key

---
+++ Introduction to Relational Databases in SQL/Chapter 4/foreign_key.sql
@@ -0,0 +1,18 @@
+-- Rename the university_shortname column
+ALTER TABLE professors
+RENAME COLUMN university_shortname TO university_id;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+
+-- Try to insert a new professor
+INSERT INTO professors (firstname, lastname, university_id)
+VALUES ('Albert', 'Einstein', 'UZH');
+
+-- Select all professors working for universities in the city of Zurich
+SELECT professors.lastname, universities.id, universities.university_city
+FROM professors
+JOIN universities
+ON professors.university_id = universities.id
+WHERE universities.university_city = 'Zurich';
