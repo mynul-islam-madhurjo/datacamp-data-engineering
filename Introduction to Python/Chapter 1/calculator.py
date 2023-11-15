36bf06220ee0ce646d58c352adb3e1713c38afac
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-11-15 09:33:01 +0600

    intro to python chapter 1 variables and types

---
+++ Introduction to Python/Chapter 1/calculator.py
@@ -0,0 +1,17 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(7+10)
+
+# Addition
+print(4+5)
+
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3*5)
+
+# Division
+print(10/2)
---
+++ Introduction to Python/Chapter 1/types&variables.py
@@ -0,0 +1,72 @@
+# int, float, str, bool
+var = 'a' + 'b'
+print(var) #ab
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Calculating with variables
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings = savings + new_savings
+
+# Print total_savings
+print(total_savings)
+
+# Other Variable Types
+
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
+
+# Operations with other types
+
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+# Type conversion
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+
+
